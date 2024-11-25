ad5df374136d3b6561d9b053ccb78f314eda0a92
RedishGaming127408 <79044366+RedishGaming127408@users.noreply.github.com>
2024-11-25 21:13:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ const CONFIG = {
     desc: 'Is it okay pu ba na to chat you?',
     btnYes: 'Yes! <33',
     btnNo: 'Nope :3',
-    question: 'Out of over 7 billion people in the world, why do you love me <3',
+    question: '',
     btnReply: 'Send to you <3',
     reply: 'Love is love, and if not love, then still love <33333333',
     mess: 'I knew it 🥰. Love you so much 😘😘',
     messDesc: 'Tonight at 7 PM, I\'ll pick you up for a date.',
     btnAccept: 'Okieeeee <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //your message link. E.g.: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://web.facebook.com/messages/t/jhian.manacpo' //your message link. E.g.: https://www.facebook.com/messages/t/100014188333536
 };
