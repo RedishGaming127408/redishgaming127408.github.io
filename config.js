7b75701f65a3f216862df6bca7e35ebed0fb7d80
RedishGaming127408 <79044366+RedishGaming127408@users.noreply.github.com>
2024-11-25 20:52:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
-const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+const CONFIG = { 
+    titleWeb: "Website Title",
+    introTitle: 'Your Lover\'s Name',
+    introDesc: `The Earth is inherently strange
+    But why do you keep taking the wrong path
+    Wandering into my lonely heart
+    Behind the word "love" is "care"`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    title: 'Did you fall in love at first sight 🥰',
+    desc: 'Did you fall for that smile from the very first moment',
+    btnYes: 'Love it <33',
+    btnNo: 'Nope :3',
+    question: 'Out of over 7 billion people in the world, why do you love me <3',
+    btnReply: 'Send to you <3',
+    reply: 'Love is love, and if not love, then still love <33333333',
+    mess: 'I knew it 🥰. Love you so much 😘😘',
+    messDesc: 'Tonight at 7 PM, I\'ll pick you up for a date.',
+    btnAccept: 'Okieeeee <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //your message link. E.g.: https://www.facebook.com/messages/t/100014188333536
+};
