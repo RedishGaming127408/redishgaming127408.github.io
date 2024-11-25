c19cebca58f2ba3a37787c442c03ec99255c6241
RedishGaming127408 <79044366+RedishGaming127408@users.noreply.github.com>
2024-11-25 22:38:40 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -115,7 +115,7 @@ $('#yes').click(function() {
         html: true,
         width: 900,
         padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder=':>'>",
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyy'>",
         background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
